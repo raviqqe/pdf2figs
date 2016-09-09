1b31df7fa600f0aa8028fb4125d98d741aca93df
Yota Toyama <raviqqe@gmail.com>
2016-09-09 16:47:58 +0900

    Add prototypes

---
+++ src/main/java/com/raviqqe/pdf2figs/App.java
@@ -0,0 +1,14 @@
+package com.raviqqe.pdf2figs;
+
+import org.apache.pdfbox.pdmodel.*;
+import java.io.*;
+
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
